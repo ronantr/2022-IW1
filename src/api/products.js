6db01d38807543da0500fc0195c80a80acb6f445
Syu <herve.tutuaku@beamy.io>
2022-05-03 14:14:22 +0200

    init project

---
+++ src/api/products.js
@@ -0,0 +1,9 @@
+import { createRequest } from "./api.js";
+
+const request = createRequest();
+
+export function getProducts() {
+  return request.get("/products")
+    .then(({ data }) => data)
+    .catch(console.error);
+}
