f2666e357e7723d329850e046b743286b8a66a49
Syu <herve.tutuaku@beamy.io>
2022-05-03 17:14:20 +0200

    indexed db

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import page from "page";
+import checkConnectivity from "network-latency";
+
 
 import { getProducts, getProduct } from './api/products';
 import "./views/app-home";
+import { getRessource, getRessources, setRessource, setRessources } from "./idbHelpers";
 
 (async (root) => {
 
   const skeleton = root.querySelector('.skeleton');
   const main = root.querySelector('main');
+
+  checkConnectivity({
+    interval: 3000,
+    threshold: 2000
+  });
+
+  let NETWORK_STATE = true;
+
+  document.addEventListener('connection-changed', ({ detail }) => {
+    NETWORK_STATE = detail;
+    if (NETWORK_STATE) {
+      document.documentElement.style.setProperty('--app-bg-color', 'royalblue');
+    } else {
+      document.documentElement.style.setProperty('--app-bg-color', '#717276');
+    }
+  });
 
   const AppHome = main.querySelector('app-home');
   const AppProduct = main.querySelector('app-product');
@@ -21,9 +40,15 @@ import "./views/app-home";
   });
 
   page('/', async () => {
-    const products = await getProducts();
+    let storedproducts = []
+    if (NETWORK_STATE) {
+      const products = await getProducts();
+      storedproducts = await setRessources(products);
+    } else {
+      storedproducts = await getRessources();
+    }
 
-    AppHome.products = products;
+    AppHome.products = storedproducts;
     AppHome.active = true;
 
     skeleton.setAttribute('hidden', 'hiddle');
@@ -31,9 +56,16 @@ import "./views/app-home";
 
   page('/product/:id', async ({ params }) => {
     await import('./views/app-product');
-    const product = await getProduct(params.id);
+    
+    let storedProduct = {};
+    if (NETWORK_STATE) {
+      const product = await getProduct(params.id);
+      storedProduct = await setRessource(product);
+    } else {
+      storedProduct = await getRessource(params.id);
+    }
 
-    AppProduct.product = product;
+    AppProduct.product = storedProduct;
     AppProduct.active = true;
 
     skeleton.setAttribute('hidden', 'hiddle');
